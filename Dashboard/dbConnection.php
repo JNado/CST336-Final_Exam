ffccd83d6ae027f87ca04180172e065a235068bd
Jeff Nadolna <jnadolna@csumb.edu>
2019-05-13 17:58:58 +0000

    Heroku DB connection changes

--- Dashboard/dbConnection.php
+++ Dashboard/dbConnection.php
@@ -4,14 +4,21 @@ function getDatabaseConnection($dbName = 'final') {
     $username = 'root';
     $password = '';
     
+    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    //     $host = $url["host"];
+    //     $dbname = substr($url["path"], 1);
+    //     $username = $url["user"];
+    //     $password = $url["pass"];
+    // } 
+    
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-    } 
-
+        // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = "us-cdbr-iron-east-02.cleardb.net";
+        $dbname = "heroku_36740c1a1136d2b";
+        $username = "b6fbae3b49e168";
+        $password = "322a8cdc";
+    }
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
     
